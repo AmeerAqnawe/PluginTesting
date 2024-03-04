3df420ca80345b1278cfd786555dcdb71973eec1
AmeerAnqawe <Ameer.Anqawe@gmail.com>
2024-03-04 15:53:50 +0100

    remove tests

--- src/test/java/org/testify/StringManipulatorTest.java
+++ src/test/java/org/testify/StringManipulatorTest.java
@@ -1,47 +1,4 @@
 package org.testify;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class StringManipulatorTest {
-
-    private StringManipulator manipulator;
-
-    @BeforeEach
-    public void setUp() {
-        manipulator = new StringManipulator();
-    }
-
-    @Test
-    public void testConcatenate() {
-        assertEquals("hello world", manipulator.concatenate("hello ", "world"));
-        assertThrows(IllegalArgumentException.class, () -> manipulator.concatenate(null, "world"));
-    }
-
-    @Test
-    public void testToUpperCase() {
-        assertEquals("HELLO", manipulator.toUpperCase("hello"));
-        assertThrows(IllegalArgumentException.class, () -> manipulator.toUpperCase(null));
-    }
-
-    @Test
-    public void testStringLength() {
-        assertEquals(5, manipulator.stringLength("hello"));
-        assertThrows(IllegalArgumentException.class, () -> manipulator.stringLength(null));
-    }
-
-    @Test
-    public void testLongestSubstringWithoutRepeatingCharacters() {
-        assertEquals(" world", manipulator.longestSubstringWithoutRepeatingCharacters("hello world"));
-        assertThrows(IllegalArgumentException.class, () -> manipulator.longestSubstringWithoutRepeatingCharacters(null));
-    }
-
-    @Test
-    public void testCountSubstringOccurrences() {
-        assertEquals("Case-sensitive count: 2, Case-insensitive count: 3", manipulator.countSubstringOccurrences("hello Hello hEllo", "ello"));
-        assertThrows(IllegalArgumentException.class, () -> manipulator.countSubstringOccurrences(null, "test"));
-    }
+class StringManipulatorTest {
 }
