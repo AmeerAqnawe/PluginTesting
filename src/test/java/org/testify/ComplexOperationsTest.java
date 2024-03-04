ef3d190d5f0dcd06ef3b30085d494f5d7105b4c1
AmeerAnqawe <Ameer.Anqawe@gmail.com>
2024-03-04 16:07:51 +0100

    cover calculate square

--- src/test/java/org/testify/ComplexOperationsTest.java
+++ src/test/java/org/testify/ComplexOperationsTest.java
@@ -1,5 +1,22 @@
 package org.testify;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 public class ComplexOperationsTest {
+
+    static ComplexOperations complexOperations;
+
+    @BeforeAll
+    static void initi(){
+        complexOperations = new ComplexOperations();
+    }
+
+    @Test
+    void test_calculateSquare(){
+        Assertions.assertEquals(9, complexOperations.calculateSquare(3));
+    }
 
 }
