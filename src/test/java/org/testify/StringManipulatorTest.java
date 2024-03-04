0e21b8e8075acae80cdd1f28b7550fb0e2ba720b
AmeerAnqawe <Ameer.Anqawe@gmail.com>
2024-03-04 16:21:59 +0100

    cover concatenate

--- src/test/java/org/testify/StringManipulatorTest.java
+++ src/test/java/org/testify/StringManipulatorTest.java
@@ -1,5 +1,21 @@
 package org.testify;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class StringManipulatorTest {
+
+    static StringManipulator stringManipulator;
+
+    @BeforeAll
+    static void init(){
+        stringManipulator = new StringManipulator();
+    }
+
+    @Test
+    void test_concatenate(){
+        Assertions.assertEquals("test", stringManipulator.concatenate("tes", "t"));
+    }
 
 }
