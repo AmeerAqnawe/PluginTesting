40229ba634ab0d87ca55efc9e9fd55969cb386e3
AmeerAnqawe <Ameer.Anqawe@gmail.com>
2024-03-04 15:56:19 +0100

    remove tests

--- src/test/java/org/testify/ComplexOperationsTest.java
+++ src/test/java/org/testify/ComplexOperationsTest.java
@@ -1,12 +1,5 @@
 package org.testify;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class ComplexOperationsTest {
 
 }
