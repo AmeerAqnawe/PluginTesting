4d7fd74e82cc992f1e446fe092b72ba4cf50eff2
Participant1 <guest001@betonie.cip.fim.uni-passau.de>
2024-03-05 13:50:16 +0100

    Add tests for moduloWithRounding

--- src/test/java/org/testify/MathUtilsTest.java
+++ src/test/java/org/testify/MathUtilsTest.java
@@ -17,7 +17,25 @@ class MathUtilsTest {
             assertTrue(mathUtils.isPositive(1));
        }
 
-       @Test public void isPositiveTestZero() {
+       @Test
+       public void isPositiveTestZero() {
              assertFalse(mathUtils.isPositive(0));
        }
+
+        @Test
+        public void moduloWithRoundingTestError() {
+           assertThrows(IllegalArgumentException.class,
+                   ()-> { MathUtils.modulusWithRounding(10, 0);},
+                   "IllegalArgumentException was supposed to be thrown."
+           );
+        }
+
+        @Test
+        public void moduloWithRoundingTestRounding() {
+           assertEquals(10, MathUtils.modulusWithRounding(9,90));
+        }
+
+        @Test void moduloWithRoundingTestNoRounding() {
+           assertEquals(0, MathUtils.modulusWithRounding(90, 9));
+        }
 }
