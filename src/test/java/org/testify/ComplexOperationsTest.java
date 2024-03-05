4f5f5fb52e84d825b117d7b2f52356641bbd5092
Participant4 <guest004@giersch.cip.fim.uni-passau.de>
2024-03-05 13:35:10 +0100

    add tests for anagram

--- src/test/java/org/testify/ComplexOperationsTest.java
+++ src/test/java/org/testify/ComplexOperationsTest.java
@@ -1,4 +1,21 @@
 package org.testify;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ComplexOperationsTest {
+
+    @Test
+    public void testIsAnagram() {
+         ComplexOperations co = new ComplexOperations();
+
+         assertTrue(co.isAnagram("earth", "heart"));
+         assertTrue(co.isAnagram("", ""));
+         assertTrue(co.isAnagram("Hall o", " Hallo"));
+
+        assertFalse(co.isAnagram("ab", "abc"));
+        assertFalse(co.isAnagram("aba", "ab"));
+    }
 }
