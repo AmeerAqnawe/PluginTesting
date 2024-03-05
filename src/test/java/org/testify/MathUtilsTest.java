dc5dcfe0ecc6e8bc7552d1daa0ddce82cecaf815
Participant1 <guest001@betonie.cip.fim.uni-passau.de>
2024-03-05 13:34:07 +0100

    Improve your code by removing the smell starting from line 80 in class MathUtils

--- src/test/java/org/testify/MathUtilsTest.java
+++ src/test/java/org/testify/MathUtilsTest.java
@@ -1,5 +1,23 @@
 package org.testify;
 
- class MathUtilsTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+       MathUtils mathUtils = new MathUtils();
+       @Test
+       public void isPositiveTestNegative() {
+           assertFalse(mathUtils.isPositive(-1));
+       }
+
+       @Test
+       public void isPositiveTestPositive() {
+            assertTrue(mathUtils.isPositive(1));
+       }
+
+       @Test public void isPositiveTestZero() {
+             assertFalse(mathUtils.isPositive(0));
+       }
 }
