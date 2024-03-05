1613f3a471a828fa1c16c53f3cc2f067609dfe08
Participant5 <guest005@margarite.cip.fim.uni-passau.de>
2024-03-05 13:50:28 +0100

    add test for isPositive of MathUtils

--- src/test/java/org/testify/MathUtilsTest.java
+++ src/test/java/org/testify/MathUtilsTest.java
@@ -1,6 +1,15 @@
 package org.testify;
 
 
- class MathUtilsTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+  @Test
+  void TestIsPositive() {
+   MathUtils mathUtils = new MathUtils();
+   Assertions.assertTrue(mathUtils.isPositive(2));
+  }
 
 }
