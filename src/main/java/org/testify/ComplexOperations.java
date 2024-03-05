5098139455867b186835b61d4a2e3a20b7044b5d
Participant3 <guest006@lolch.cip.fim.uni-passau.de>
2024-03-05 13:06:27 +0100

    dummy

--- src/main/java/org/testify/ComplexOperations.java
+++ src/main/java/org/testify/ComplexOperations.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * The {@code ComplexOperations} class provides methods for performing various mathematical operations,
  * such as calculating the square and cube of a number, checking if a number is prime, calculating the factorial
- * of a number, checking if a string is a palindrome, and generating a list of prime numbers.
+ * of a number, checking iff a string is a palindrome, and generating a list of prime numbers.
  */
 public class ComplexOperations {
 
