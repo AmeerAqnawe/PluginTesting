7c121e522f3f24f1aea9f197b44b777414866771
Participant5 <guest005@margarite.cip.fim.uni-passau.de>
2024-03-05 13:36:02 +0100

    add test for makePayment of PaymentService

--- src/test/java/org/testify/PaymentServiceTest.java
+++ src/test/java/org/testify/PaymentServiceTest.java
@@ -1,5 +1,18 @@
 package org.testify;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testify.entity.BankService;
+
 class PaymentServiceTest {
+
+    @Test
+    void TestMakePayment() {
+        BankService bankService = new BankService();
+        PaymentService paymentService = new PaymentService(bankService);
+
+        Boolean result = paymentService.makePayment("1234", "4321", 12);
+        Assertions.assertTrue(result);
+    }
 
 }
