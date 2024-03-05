6cd42de866945f23c36b2c23cbf40dc87a5c65f9
Participant5 <guest005@margarite.cip.fim.uni-passau.de>
2024-03-05 13:26:19 +0100

    add test for stringLength of StringManipulator

--- src/test/java/org/testify/StringManipulatorTest.java
+++ src/test/java/org/testify/StringManipulatorTest.java
@@ -1,5 +1,15 @@
 package org.testify;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class StringManipulatorTest {
 
+
+    @Test
+    void TeststringLength() {
+        StringManipulator manipulator = new StringManipulator();
+        String testString = "test";
+        Assertions.assertEquals(4, manipulator.stringLength(testString));
+    }
 }
